264172b634579183aa38680eaf111437fb1c79e0
samplebug <49753953+samplebug@users.noreply.github.com>
2023-11-26 22:14:23 +0200

    setup done

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 
 // Add services to the container.
 
@@ -10,14 +10,26 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+/*     options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder
+                    .AllowAnyMethod()
+                    //.AllowCredentials()
+                    .SetIsOriginAllowed((host) => true)
+                    .WithOrigins("http://localhost:4200/")
+                    .AllowAnyHeader());
+        }); */
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+app.UseCors(corsPolicyBuilder => corsPolicyBuilder.WithOrigins("https://localhost:4200").AllowAnyHeader().AllowAnyMethod() ); //"CorsPolicy");
 
-app.UseAuthorization();
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
